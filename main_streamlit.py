96e64ec610f145d814a3fda6cb896a8be6584f98
Nicolas Cortinas Rožić <55030635+nicolas2192@users.noreply.github.com>
2022-12-19 23:36:46 +0400

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -28,5 +28,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# Turn JSON into df
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Print JSON df
+streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.text('With ❤️ by Nico')
