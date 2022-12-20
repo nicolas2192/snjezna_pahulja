12ffeacbdbb5b51b9b41d418ea8251727049c9de
Nicolas Cortinas Rožić <55030635+nicolas2192@users.noreply.github.com>
2022-12-20 12:47:16 +0400

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -44,5 +44,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_rows)
 
+# Add new fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 streamlit.text('With ❤️ by Nico')
